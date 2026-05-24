34fd41bcf0c680f8e8ebdbfe1a2829990e0a9ad7
NotExotic1 <133250795+NotExotic1@users.noreply.github.com>
2026-05-24 18:05:17 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Server running"
+
+@app.route("/api/playerdata", methods=["POST"])
+def playerdata():
+    data = request.json
+    print(data)
+    return jsonify({"status": "ok"})
